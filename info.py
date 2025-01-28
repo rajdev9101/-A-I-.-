2e3a61b601523e031241586bc125983dabbb9b33
rajdev9101 <devraj93957@gmail.com>
2025-01-28 23:34:06 +0530

    Update info.py

--- info.py
+++ info.py
@@ -4,15 +4,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = environ.get("API_ID" , "2099007")
-API_HASH = environ.get("API_HASH" , "b1b3d7b3b1b3d7b3b1b3d7b3b1b3d7b3")
-BOT_TOKEN = environ.get("BOT_TOKEN" , "2099007:AAH1b3d7b3b1b3d7b3b1b3d7b3b1b3d7b3")
-ADMIN = int(environ.get("ADMIN" , "1847899007"))
-CHAT_GROUP = int(environ.get("CHAT_GROUP", "-100"))
-LOG_CHANNEL = environ.get("LOG_CHANNEL", "-100")
+API_ID = environ.get("API_ID" , "27084955")
+API_HASH = environ.get("API_HASH" , "91c88b554ab2a34f8b0c72228f06fc0b")
+BOT_TOKEN = environ.get("BOT_TOKEN" , "")
+ADMIN = int(environ.get("ADMIN" , "5804953849"))
+CHAT_GROUP = int(environ.get("CHAT_GROUP", "-1002255543570"))
+LOG_CHANNEL = environ.get("LOG_CHANNEL", "-1002390669882")
 MONGO_URL = environ.get("MONGO_URL" , "mongodb://localhost:27017")
 AUTH_CHANNEL = int(
-    environ.get("AUTH_CHANNEL", "-1001734958816")
+    environ.get("AUTH_CHANNEL", "-1002465960701")
 )
 FSUB = environ.get("FSUB", True)
 STICKERS_IDS = (
